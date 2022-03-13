0e01f7ffa98fadeef26b148d2f396ebf94f6f31c
FIFOFridge <FIFOFridgee@outlook.com>
2022-03-13 16:54:32 +0100

    Fixed tailwind JIT issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    TAILWIND_MODE: 'build' // fix for JIT issue during development: https://github.com/tailwindlabs/tailwindcss/issues/6847
 };
 
 /*
